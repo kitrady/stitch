66d4cec9a27368a39656d04067b54e29d34d727a
Kit Rady <kitprady@gmail.com>
2024-11-24 11:38:49 -0600

    fixing-typos

--- PatternRunner.java
+++ PatternRunner.java
@@ -1,16 +1,35 @@
 public class PatternRunner {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    InputGetter objInputGetter = new InputGetter(1.2, 4, 4);
-    InputConverter objInputConverter = new InputConverter(objInputGetter.getRadius(), objInputGetter.getGauge(), objInputGetter.getVertGauge());
-    SphereMaker objSphereMaker = new SphereMaker(objInputConverter.getStRadius(), objInputConverter.getRowCircumference());
+        InputGetter objInputGetter = new InputGetter(1.2, 4, 4);
+        InputConverter objInputConverter = new InputConverter(objInputGetter.getRadius(), objInputGetter.getGauge(), objInputGetter.getVertGauge());
+        SphereMaker objSphereMaker = new SphereMaker(objInputConverter.getStRadius(), objInputConverter.getRowCircumference());
 
-    System.out.println("\nobjInputGetter:" + objInputGetter);
-    System.out.println("\nobjInputConverter:" + objInputConverter);
-    System.out.println("\nobjSphereMaker:" + objSphereMaker);
-    
-    objSphereMaker.printRows();
+        System.out.println("\nobjInputGetter:" + objInputGetter);
+        System.out.println("\nobjInputConverter:" + objInputConverter);
+        System.out.println("\nobjSphereMaker:" + objSphereMaker);
 
-    // TODO - make it properly handle middle row (aka repeat or not)
-  }
+        objSphereMaker.printRows();
+
+        // TODO
+        //  - make it properly handle middle row (aka repeat or not)
+        //  - also make handle input methods be one method that takes messages as parameters
+
+        // What is crochet-pattern-maker.iml?
+        // it is essentially IDE config file
+        // if not in project, will have to open new project in Intellij each time I pull from repo
+        // but also changes a lot, and makes things really annoying when many people are working on same repo
+        // this is what build tools like gradle are for
+        // so for now add it to repo, but ask dad about it later
+
+        // What is .idea folder?
+        // it is essentially project config files
+        // if not in project, ppl may have different config settings and could be problematic
+        // but also changes a lot, and makes things really annoying when many people are working on same repo
+        // so for now add it to repo, but ask dad about it later
+
+        // what is out folder?
+        // basically just the compiled code
+        // don't add it to repo
+    }
 }
