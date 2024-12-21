df79cc63228adc84a7d39eb3442385334e4ade88
Kit Rady <kitprady@gmail.com>
2024-12-20 19:19:00 -0600

    more sophisticated formatting and tests

--- app/src/main/java/com/kitrady/PatternFormatter.java
+++ app/src/main/java/com/kitrady/PatternFormatter.java
@@ -1,51 +1,125 @@
 package com.kitrady;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class PatternFormatter {
-    private ArrayList<Integer> stitchesPerRow = new ArrayList<Integer>();
+    private ArrayList<Integer> stitchesPerRd = new ArrayList<Integer>();
     private ArrayList<String> formattedPattern = new ArrayList<String>();
 
-    public PatternFormatter(ArrayList<Integer> stitchesPerRow) {
-        this.stitchesPerRow = stitchesPerRow;
+    public PatternFormatter(ArrayList<Integer> stitchesPerRd) {
+        this.stitchesPerRd = stitchesPerRd;
     }
 
     private void formatPattern() {
-        formattedPattern.add("Rd 1: " + stitchesPerRow.getFirst() + " sc in magic ring (" + stitchesPerRow.getFirst() + ")");
+        // creates first round separately because of magic ring technique
+        formattedPattern.add("Rd 1: " + stitchesPerRd.getFirst() + " sc in magic ring (" + stitchesPerRd.getFirst() + ")");
 
-        for (int i = 1; i < stitchesPerRow.size(); i++) {
-            String currentRow = "Rd " + (i + 1) + ": ";
+        int FinalLargestRdIndex = stitchesPerRd.lastIndexOf(Collections.max(stitchesPerRd)); // finds last round that could need increases
+        formatIncreaseRounds(FinalLargestRdIndex);
+        formatDecreaseRounds(FinalLargestRdIndex);
+    }
 
-            int previousStTotal = stitchesPerRow.get(i - 1);
-            int currentStTotal = stitchesPerRow.get(i);
+    public void formatIncreaseRounds(int FinalLargestRdIndex) {
+        // loops to create each round of pattern that needs increases
+        for (int i = 1; i < FinalLargestRdIndex; i++) {
+            String currentRound = "Rd " + (i + 1) + ": "; // adds round number
+
+            int previousStTotal = stitchesPerRd.get(i - 1);
+            int currentStTotal = stitchesPerRd.get(i);
             int numIncreases = currentStTotal - previousStTotal;
 
-            if (numIncreases != 0) {
+            // checks if any increases are needed
+            if (numIncreases > 0) {
                 int numStitchesInRepeat = currentStTotal / numIncreases;
 
+                // if increases don't divide evenly into stitch total, calculates remaining stitches
                 int extraStitches = 0;
                 if (numStitchesInRepeat * numIncreases != currentStTotal) {
                     extraStitches = currentStTotal - (numStitchesInRepeat * numIncreases);
                 }
 
-                currentRow += "(" + (numStitchesInRepeat - 2) + "sc, inc) x" + numIncreases;
-
-                if (extraStitches != 0) {
-                    currentRow += ", " + extraStitches + " sc";
+                // checks if any single crochets are needed
+                if (numStitchesInRepeat - 2 > 0) {
+                    currentRound += "(" + (numStitchesInRepeat - 2) + "sc, inc) x" + numIncreases;
                 }
-            } else {
-                currentRow += "sc in each st in round";
+                // round is all increases if no single crochets are needed
+                else {
+                    currentRound += "inc in each st in round";
+                }
+
+                // adds extra stitches if there are any
+                if (extraStitches > 0) {
+                    currentRound += ", " + extraStitches + " sc";
+                }
+            }
+            // round is all single crochets if no increases are needed
+            else {
+                currentRound += "sc in each st in round";
             }
 
-            currentRow += " (" + currentStTotal + ")";
-            formattedPattern.add(currentRow);
+            // adds total stitch count to end
+            currentRound += " (" + currentStTotal + ")";
+
+            // adds the round to the collection of rounds
+            formattedPattern.add(currentRound);
         }
+    }
+
+    public void formatDecreaseRounds(int FinalLargestRdIndex) {
+        for (int i = FinalLargestRdIndex; i < stitchesPerRd.size(); i++) {
+            String currentRound = "Rd " + (i + 1) + ": "; // adds round number
+
+            int previousStTotal = stitchesPerRd.get(i - 1);
+            int currentStTotal = stitchesPerRd.get(i);
+            int numDecreases = previousStTotal - currentStTotal;
+
+            // checks if any decreases are needed
+            if (numDecreases > 0) {
+                int numStitchesInRepeat = previousStTotal / numDecreases;
+
+                // if decreases don't divide evenly into stitch total, calculates remaining stitches
+                int extraStitches = 0;
+                if (numStitchesInRepeat * numDecreases != previousStTotal) {
+                    extraStitches = previousStTotal - (numStitchesInRepeat * numDecreases);
+                }
+
+                // checks if any single crochets are needed
+                if (numStitchesInRepeat - 2 > 0) {
+                    currentRound += "(" + (numStitchesInRepeat - 2) + "sc, dec) x" + numDecreases;
+                }
+                // round is all decreases if no single crochets are needed
+                else {
+                    currentRound += "dec in each st in round";
+                }
+
+                // adds extra stitches if there are any
+                if (extraStitches > 0) {
+                    currentRound += ", " + extraStitches + " sc";
+                }
+            }
+            // round is all single crochets if no decreases are needed
+            else {
+                currentRound += "sc in each st in round";
+            }
+
+            // adds total stitch count to end
+            currentRound += " (" + currentStTotal + ")";
+
+            // adds the round to the collection of rounds
+            formattedPattern.add(currentRound);
+        }
+    }
+
+    public ArrayList<String> getFormattedPattern() {
+        formatPattern();
+        return formattedPattern;
     }
 
     public void printPattern() {
         formatPattern();
-        for (int i = 0; i < formattedPattern.size(); i++) {
-            System.out.print("\n" + formattedPattern.get(i));
+        for (String s : formattedPattern) {
+            System.out.print("\n" + s);
         }
     }
 }
--- app/src/main/java/com/kitrady/SphereMaker.java
+++ app/src/main/java/com/kitrady/SphereMaker.java
@@ -4,54 +4,47 @@ import java.util.ArrayList;
 import java.lang.Math;
 
 public class SphereMaker {
-    private ArrayList<Integer> stitchesPerRow = new ArrayList<Integer>(); // array list where index represents row and value is number of stitches in row
+    private ArrayList<Integer> stitchesPerRd = new ArrayList<Integer>(); // array list where index represents round and value is number of stitches in round
     private final double stRadius;
     private final double rowCircumference;
-    private final double degreesPerRow;
+    private final double degreesPerRd;
 
     public SphereMaker(double stRadius, double rowCircumference) {
         this.stRadius = stRadius; // radius measured in stitches
         this.rowCircumference = rowCircumference;  // circumference measured in rows
-        degreesPerRow = 360.0 / rowCircumference; // divides 360 degrees in circumference by rows in circumference to get degrees per row
+        degreesPerRd = 360.0 / rowCircumference; // divides 360 degrees in circumference by rows in circumference to get degrees per row
     }
 
-    private void generateRows() {
-        // computes stitches per row, starting from top of sphere (aka 90 degrees) and ending at side of sphere (aka 0 degrees)
-        // uses degrees per row to increment angle properly
-        // no row can be at exact top, so starts with one row offset
+    private void generateRounds() {
+        // computes stitches per round, starting from top of sphere (aka 90 degrees) and ending at side of sphere (aka 0 degrees)
+        // uses degrees per round to increment angle properly
+        // no round can be at exact top, so starts with one round offset
         double angle;
-        for (angle = (90 - degreesPerRow); angle >= 0; angle -= degreesPerRow) {
-            double currentRadius = Math.cos(Math.toRadians(angle)) * stRadius; // finds radius associated with current row/angle using cosine
-            double currentCircumference = 6.2831 * currentRadius; // finds circumference associated with current row/angle using circumference formula
-            stitchesPerRow.add((int) Math.round(currentCircumference)); // expresses circumference as whole number of stitches and adds to list of stitches per row
+        for (angle = (90 - degreesPerRd); angle >= 0; angle -= degreesPerRd) {
+            double currentRadius = Math.cos(Math.toRadians(angle)) * stRadius; // finds radius associated with current round/angle using cosine
+            double currentCircumference = 6.2831 * currentRadius; // finds circumference associated with current round/angle using circumference formula
+            stitchesPerRd.add((int) Math.round(currentCircumference)); // expresses circumference as whole number of stitches and adds to list of stitches per round
         }
 
-        int originalSize = stitchesPerRow.size();
-        // if more than half of the angle increment was left, there is a "missing" row
-        if (angle + degreesPerRow >= degreesPerRow / 2) {
-            stitchesPerRow.add((int) Math.round(6.2831 * stRadius)); // adds "missing" row using an angle of zero, aka full radius
+        int originalSize = stitchesPerRd.size();
+        // if more than half of the angle increment was left, there is a "missing" round
+        if (angle + degreesPerRd >= degreesPerRd / 2) {
+            stitchesPerRd.add((int) Math.round(6.2831 * stRadius)); // adds "missing" round using an angle of zero, aka full radius
         }
         // since a sphere has identical hemispheres, reverse current stitch counts to get rest of sphere
         for (int i = 0; i < originalSize; i++) {
-            stitchesPerRow.add(stitchesPerRow.get(originalSize - i - 1));
+            stitchesPerRd.add(stitchesPerRd.get(originalSize - i - 1));
         }
     }
 
-    public ArrayList<Integer> getStitchesPerRow() {
-        generateRows();
-        return stitchesPerRow;
-    }
-
-    public void printRows() {
-        generateRows();
-        for (int i = 0; i < stitchesPerRow.size(); i++) {
-            System.out.print("\nRow " + (i + 1) + ": " + stitchesPerRow.get(i) + " stitches");
-        }
+    public ArrayList<Integer> getStitchesPerRd() {
+        generateRounds();
+        return stitchesPerRd;
     }
 
     public String toString() {
         return ("\n- Radius in stitches = " + stRadius +
                 "\n- Circumference in rows = " + rowCircumference +
-                "\n- Degrees per row = " + degreesPerRow);
+                "\n- Degrees per round = " + degreesPerRd);
     }
 }
