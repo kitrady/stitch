c6886bd05a1bd69961ceefacbc2b81b83ce88b01
Kit Rady <kitprady@gmail.com>
2025-01-16 14:53:01 -0600

    making a branch for decrease formatting

--- app/src/main/java/com/kitrady/PatternFormatter.java
+++ app/src/main/java/com/kitrady/PatternFormatter.java
@@ -123,6 +123,106 @@ public class PatternFormatter {
         }
     }
 
+    public void formatDecreaseRounds(int finalLargestRdIndex) {
+        for (int i = finalLargestRdIndex; i < stitchesPerRd.size(); i++) {
+            // current round is the string containing the round that is currently being built
+            String currentRound = "Rd " + (i + 1) + ": "; // adds round number to the current round
+
+            ArrayList<Integer> roundComponentCounts = new ArrayList<>(); // holds all the counts associated with the components of the round
+            ArrayList<StitchTypes> roundComponentTypes = new ArrayList<>(); // hold the types of each component of the round
+            // the above components will be added to the current round once they are fully formatted
+
+            int previousStTotal = stitchesPerRd.get(i - 1);
+            int currentStTotal = stitchesPerRd.get(i);
+            int numDecreases = previousStTotal - currentStTotal;
+
+            // checks if any decreases are needed
+            if (numDecreases > 0) {
+                int numStitchesInSection = previousStTotal / numDecreases;
+                int numScInSection = numStitchesInSection - 2;
+
+                // if decreases don't divide evenly into stitch total, calculates remaining stitches
+                int extraStitches = 0;
+                if (numStitchesInSection * numDecreases != previousStTotal) {
+                    extraStitches = previousStTotal - (numStitchesInSection * numDecreases);
+                }
+
+                // checks if any single crochets are needed
+                if (numScInSection > 0) {
+                    // checks if round should have alternate format where one of the sections is split in half
+                    if (alternateRdCounter % 2 == 0 && numDecreases > 1) {
+                        int numScInHalfSection = numScInSection / 2; // finds sc in each part of half section
+
+                        // adds first piece of half section to round components
+                        roundComponentCounts.add(numScInHalfSection);
+                        roundComponentTypes.add(StitchTypes.SC);
+                        roundComponentCounts.add(1);
+                        roundComponentTypes.add(StitchTypes.DEC);
+
+                        // adds the sc that are part of the repeat to the round components
+                        roundComponentCounts.add(numScInSection);
+                        roundComponentTypes.add(StitchTypes.REPEAT_SC);
+
+                        // adds the dec that are part of the repeat to the round components
+                        roundComponentCounts.add(1);
+                        roundComponentTypes.add(StitchTypes.REPEAT_DEC);
+
+                        // adds number of times the repeat is done to the round components
+                        roundComponentCounts.add(numDecreases - 1);
+                        roundComponentTypes.add(StitchTypes.REPEAT_COUNT);
+
+                        // checks if truncation occurred when finding number of sc in half section
+                        if (numScInHalfSection * 2 != numScInSection) {
+                            extraStitches += 1; // if truncation did occur, add an extra stitch
+                        }
+
+                        // adds second piece of half section to round components
+                        roundComponentCounts.add(numScInHalfSection);
+                        roundComponentTypes.add(StitchTypes.SC);
+                    }
+                    // otherwise, uses normal format
+                    else {
+                        // adds the sc that are part of the repeat to the round components
+                        roundComponentCounts.add(numScInSection);
+                        roundComponentTypes.add(StitchTypes.REPEAT_SC);
+
+                        // adds the dec that are part of the repeat to the round components
+                        roundComponentCounts.add(1);
+                        roundComponentTypes.add(StitchTypes.REPEAT_DEC);
+
+                        // adds number of times the repeat is done to the round components
+                        roundComponentCounts.add(numDecreases);
+                        roundComponentTypes.add(StitchTypes.REPEAT_COUNT);
+                    }
+                    alternateRdCounter++;
+                }
+                // round is all decreases if no single crochets are needed
+                else {
+                    currentRound += "dec in each st in round";
+                }
+
+                // adds extra stitches if there are any
+                if (extraStitches > 0) {
+                    // adds extra stitches to round components
+                    roundComponentCounts.add(extraStitches);
+                    roundComponentTypes.add(StitchTypes.SC);
+                }
+            }
+            // round is all single crochets if no decreases are needed
+            else {
+                currentRound += "sc in each st in round";
+            }
+
+            currentRound += assembleRoundComponents(roundComponentCounts, roundComponentTypes);
+
+            // adds total stitch count to end
+            currentRound += " (" + currentStTotal + ")";
+
+            // adds the round to the collection of rounds
+            formattedPattern.add(currentRound);
+        }
+    }
+
     public String assembleRoundComponents(ArrayList<Integer> stCounts, ArrayList<StitchTypes> stTypes) {
         StringBuilder currentRound = new StringBuilder(); // the round that is currently being assembled
         StitchTypes previousType = null; // the type of the previous component
@@ -151,12 +251,26 @@ public class PatternFormatter {
                         }
                         break;
 
+                    case DEC: // if the current component is a dec
+                        if (previousType == null) { // and there were NO sc preceding the dec
+                            currentRound.append("dec"); // add the formatted dec to the current round
+                        }
+                        else { // and there were sc preceding the dec
+                            currentRound.append(scTotal).append(" sc"); // add the formatted sc to the current round
+                            scTotal = 0; // reset the running sc total
+                            currentRound.append(", dec"); // and add the formatted dec to the current round
+                        }
+                        break;
+
                     case REPEAT_SC: // if the current component is a sc that could be part of a repeat
                         possiblyRepeatedSc = currentComponentCount; // add it to the possibly repeated single crochets
                         break;
 
-                    case REPEAT_INC: // if the current component is a sc that could be part of a repeat
+                    case REPEAT_INC: // if the current component is an inc that could be part of a repeat
                         break; // nothing needs to happen as inc are always in groups of one
+
+                    case REPEAT_DEC: // if the current component is a dec that could be part of a repeat
+                        break; // nothing needs to happen as dec are always in groups of one
 
                     case REPEAT_COUNT: // if the current component is the number of times the repeat is done
                         if (currentComponentCount == 1) { // and that number is one
@@ -164,11 +278,21 @@ public class PatternFormatter {
                             scTotal += possiblyRepeatedSc;
                             currentRound.append(scTotal).append(" sc");
                             scTotal = 0;
-                            currentRound.append(", inc");
+                            if (previousType == StitchTypes.REPEAT_INC) { // checks previous type to know whether to add inc or dec
+                                currentRound.append(", inc");
+                            }
+                            else {
+                                currentRound.append(", dec");
+                            }
                         }
                         else { // and that number is greater than one
                             // add the formatted repeat to the current round
-                            currentRound.append("(").append(possiblyRepeatedSc).append(" sc, inc) x").append(currentComponentCount);
+                            if (previousType == StitchTypes.REPEAT_INC) { // checks previous type to know whether to add inc or dec
+                                currentRound.append("(").append(possiblyRepeatedSc).append(" sc, inc) x").append(currentComponentCount);
+                            }
+                            else {
+                                currentRound.append("(").append(possiblyRepeatedSc).append(" sc, dec) x").append(currentComponentCount);
+                            }
                         }
                 }
             }
@@ -188,22 +312,43 @@ public class PatternFormatter {
                         currentRound.append(", inc");
                         break;
 
+                    case DEC:
+                        if (previousType != null && previousType.equals(StitchTypes.SC)) {
+                            currentRound.append(", ").append(scTotal).append(" sc");
+                            scTotal = 0;
+                        }
+                        currentRound.append(", dec");
+                        break;
+
                     case REPEAT_SC:
                         possiblyRepeatedSc = currentComponentCount;
                         break;
 
                     case REPEAT_INC:
-                        break;
+                        break; // nothing needs to happen as inc are always in groups of one
+
+                    case REPEAT_DEC:
+                        break; // nothing needs to happen as dec are always in groups of one
 
                     case REPEAT_COUNT:
-                        if (currentComponentCount == 1) {
+                        if (currentComponentCount < 2) {
                             scTotal += possiblyRepeatedSc;
                             currentRound.append(", ").append(scTotal).append(" sc");
                             scTotal = 0;
-                            currentRound.append(", inc");
+                            if (previousType == StitchTypes.REPEAT_INC) {
+                                currentRound.append(", inc");
+                            }
+                            else {
+                                currentRound.append(", dec");
+                            }
                         }
                         else {
-                            currentRound.append(", (").append(possiblyRepeatedSc).append(" sc, inc) x").append(currentComponentCount);
+                            if (previousType == StitchTypes.REPEAT_INC) {
+                                currentRound.append(", (").append(possiblyRepeatedSc).append(" sc, inc) x").append(currentComponentCount);
+                            }
+                            else {
+                                currentRound.append(", (").append(possiblyRepeatedSc).append(" sc, dec) x").append(currentComponentCount);
+                            }
                         }
                 }
             }
@@ -214,75 +359,6 @@ public class PatternFormatter {
             currentRound.append(", ").append(scTotal).append(" sc");
         }
         return currentRound.toString();
-    }
-
-    // TODO make this use arrays
-    public void formatDecreaseRounds(int finalLargestRdIndex) {
-        for (int i = finalLargestRdIndex; i < stitchesPerRd.size(); i++) {
-            String currentRound = "Rd " + (i + 1) + ": "; // adds round number
-
-            int previousStTotal = stitchesPerRd.get(i - 1);
-            int currentStTotal = stitchesPerRd.get(i);
-            int numDecreases = previousStTotal - currentStTotal;
-
-            // checks if any decreases are needed
-            if (numDecreases > 0) {
-                int numStitchesInSection = previousStTotal / numDecreases;
-                int numScInSection = numStitchesInSection - 2;
-
-                // if decreases don't divide evenly into stitch total, calculates remaining stitches
-                int extraStitches = 0;
-                if (numStitchesInSection * numDecreases != previousStTotal) {
-                    extraStitches = previousStTotal - (numStitchesInSection * numDecreases);
-                }
-
-                // checks if any single crochets are needed
-                if (numScInSection > 0) {
-                    // checks if there are multiple sections in the round
-                    if (numDecreases > 1) {
-                        if (alternateRdCounter % 2 == 0) {
-                            int numScInHalfSection = numScInSection / 2; // finds sc in each part of half section
-
-                            // adds "second" half of sc to extra stitches, just because this is a convenient way to incorporate the stitches
-                            extraStitches += numScInHalfSection;
-
-                            // checks if truncation occurred when finding number of sc in half section
-                            if (numScInHalfSection * 2 != numScInSection) {
-                                extraStitches += 1; // if truncation did occur, add an extra stitch
-                            }
-                            currentRound += numScInHalfSection + " sc, dec, (" + numScInSection + " sc, dec) x" + (numDecreases - 1);
-                        }
-                        else {
-                            currentRound += "(" + (numStitchesInSection - 2) + " sc, dec) x" + numDecreases;
-                        }
-                        alternateRdCounter++;
-                    }
-                    // otherwise, there is only one section
-                    else {
-                        currentRound += numScInSection + " sc, dec";
-                    }
-                }
-                // round is all decreases if no single crochets are needed
-                else {
-                    currentRound += "dec in each st in round";
-                }
-
-                // adds extra stitches if there are any
-                if (extraStitches > 0) {
-                    currentRound += ", " + extraStitches + " sc";
-                }
-            }
-            // round is all single crochets if no decreases are needed
-            else {
-                currentRound += "sc in each st in round";
-            }
-
-            // adds total stitch count to end
-            currentRound += " (" + currentStTotal + ")";
-
-            // adds the round to the collection of rounds
-            formattedPattern.add(currentRound);
-        }
     }
 
     public ArrayList<String> getFormattedPattern() {
