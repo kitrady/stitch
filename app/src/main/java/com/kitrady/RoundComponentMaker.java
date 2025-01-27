3c071163f4f2d6f3c592a98803143fadb21594a7
Kit Rady <kitprady@gmail.com>
2025-01-27 16:45:39 -0600

    saving progress

--- app/src/main/java/com/kitrady/RoundComponentMaker.java
+++ app/src/main/java/com/kitrady/RoundComponentMaker.java
@@ -7,24 +7,25 @@ public class RoundComponentMaker {
     private final ArrayList<Integer> stitchesPerRound;
     private final ArrayList<String> formattedPattern = new ArrayList<>();
     private int alternateRoundToPreventBubblesCounter = 1;
+    private RoundComponentAssembler assembler;
 
     public RoundComponentMaker(ArrayList<Integer> stitchesPerRound) {
         this.stitchesPerRound = stitchesPerRound;
     }
 
-    private void formatPattern() {
+    public void formatPattern() {
         formattedPattern.add("Rd 1: " + stitchesPerRound.getFirst() + " sc in magic ring (" + stitchesPerRound.getFirst() + ")");
 
         int finalLargestRoundIndex = stitchesPerRound.lastIndexOf(Collections.max(stitchesPerRound));
-        formatIncreaseRounds(finalLargestRoundIndex);
-        formatDecreaseRounds(finalLargestRoundIndex);
+        formatIncreaseRounds(stitchesPerRound, finalLargestRoundIndex);
+        formatDecreaseRounds(stitchesPerRound, finalLargestRoundIndex);
     }
 
-    public void formatIncreaseRounds(int finalLargestRoundIndex) {
+    private void formatIncreaseRounds(ArrayList<Integer> stitchesPerRound, int finalLargestRoundIndex) {
         for (int i = 1; i < finalLargestRoundIndex; i++) {
             String inProgressRound = "Rd " + (i + 1) + ": ";
 
-            RoundComponentAssembler assembler = new RoundComponentAssembler();
+            assembler = new RoundComponentAssembler();
 
             int previousStitchTotal = stitchesPerRound.get(i - 1);
             int currentStitchTotal = stitchesPerRound.get(i);
@@ -69,6 +70,11 @@ public class RoundComponentMaker {
                         assembler.updateRoundComponents(numIncreases, ComponentTypes.REPEAT_COUNT);
                     }
                     alternateRoundToPreventBubblesCounter++;
+                    // TODO: the below comments describe an issue
+                    // if increases in not greater than one, may be three normal rounds in a row
+                    // or two in a row depending on whether you count 1 inc round to be normal
+                    // solution is to either not increment the counter when inc <= 1
+                    // or create code to format round as "# sc, inc # sc"
                 }
 
                 if (extraStitches > 0) {
@@ -84,11 +90,11 @@ public class RoundComponentMaker {
         }
     }
 
-    public void formatDecreaseRounds(int finalLargestRdIndex) {
+    private void formatDecreaseRounds(ArrayList<Integer> stitchesPerRound, int finalLargestRdIndex) {
         for (int i = finalLargestRdIndex; i < stitchesPerRound.size(); i++) {
             String inProgressRound = "Rd " + (i + 1) + ": ";
 
-            RoundComponentAssembler assembler = new RoundComponentAssembler();
+            assembler = new RoundComponentAssembler();
 
             int previousStitchTotal = stitchesPerRound.get(i - 1);
             int currentStitchTotal = stitchesPerRound.get(i);
@@ -149,15 +155,25 @@ public class RoundComponentMaker {
         }
     }
 
+    public RoundComponentAssembler getAssembler() {
+        return assembler;
+    }
+
     public ArrayList<String> getFormattedPattern() {
-        formatPattern();
         return formattedPattern;
     }
 
     public void printPattern() {
-        formatPattern();
         for (String s : formattedPattern) {
             System.out.print("\n" + s);
+        }
+    }
+
+    public void formatGivenRounds(ArrayList<Integer> stitchesPerRound, boolean isDecrease) {
+        if (isDecrease) {
+            formatDecreaseRounds(stitchesPerRound, 1);
+        } else {
+            formatIncreaseRounds(stitchesPerRound, stitchesPerRound.size());
         }
     }
 }
