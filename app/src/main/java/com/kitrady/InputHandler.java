91870721b564464f90e472f79b0c3e4ad6176c2f
Kit Rady <kitrady@gmail.com>
2025-02-14 10:12:40 -0600

    improving sphere making

--- app/src/main/java/com/kitrady/InputHandler.java
+++ app/src/main/java/com/kitrady/InputHandler.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import static java.lang.Math.*;
 
 public class InputHandler {
+    private final double diameter; // units are inches
     private final double radius; // units are inches
     private final double gauge; // units are stitches per inch
     private final double vertGauge; // units are rows per inch
@@ -13,9 +14,9 @@ public class InputHandler {
     private final double circumferenceInRounds; // units are rounds
 
     public InputHandler(Scanner input) {
-        radius = abs(handleInput(input,
-                "\nEnter the radius of your sphere in inches: ",
-                "\nPlease enter just a number that is the radius of your sphere in inches."));
+        diameter = abs(handleInput(input,
+                "\nEnter the diameter of your sphere in inches: ",
+                "\nPlease enter just a number that is the diameter of your sphere in inches."));
         input.nextLine();
 
         gauge = abs(handleInput(input,
@@ -28,13 +29,15 @@ public class InputHandler {
                 "\nPlease enter just a number that is your vertical crochet gauge in stitches per inch."));
         input.nextLine();
 
+        radius = diameter / 2;
         radiusInStitches = radius * gauge;
         radiusInRounds = radius * vertGauge;
         circumferenceInRounds = 6.2831 * radiusInRounds;
     }
 
-    public InputHandler(double radius, double gauge, double vertGauge) {
-        this.radius = abs(radius);
+    public InputHandler(double diameter, double gauge, double vertGauge) {
+        this.diameter = abs(diameter);
+        this.radius = this.diameter / 2;
         this.gauge = abs(gauge);
         this.vertGauge = abs(vertGauge);
         radiusInStitches = radius * gauge;
