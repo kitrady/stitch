10a4a16c339478c0bbc5ca11461c9626f16434cf
Kit Rady <kitrady@gmail.com>
2025-02-25 14:54:08 -0600

    can generate stitch totals for half ellipsoid

---
+++ app/src/test/java/com/kitrady/HalfEllipsoidInputHandlerTest.java
@@ -0,0 +1,75 @@
+package com.kitrady;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HalfEllipsoidInputHandlerTest {
+    @Test
+    public void testHardcodedInputs() {
+        HalfEllipsoidInputHandler handler = new HalfEllipsoidInputHandler(2.4, 5.3, 4.3, 3.4);
+        assertEquals(2.4, handler.getDiameterInInches());
+        assertEquals(5.3, handler.getHeightInInches());
+        assertEquals(4.3, handler.getStitchGauge());
+        assertEquals(3.4, handler.getRoundGauge());
+    }
+
+    @Test
+    public void testNegativeHardcodedInputs() {
+        HalfEllipsoidInputHandler handler = new HalfEllipsoidInputHandler(-2, -5.3, -4.3, -3);
+        assertEquals(2, handler.getDiameterInInches());
+        assertEquals(5.3, handler.getHeightInInches());
+        assertEquals(4.3, handler.getStitchGauge());
+        assertEquals(3, handler.getRoundGauge());
+    }
+
+    @Test
+    public void testScannerInputs() {
+        String data = "3\n5\n4\n3\n";
+        Scanner input = new Scanner(data);
+        HalfEllipsoidInputHandler handler = new HalfEllipsoidInputHandler(input);
+        assertEquals(3, handler.getDiameterInInches());
+        assertEquals(5, handler.getHeightInInches());
+        assertEquals(4, handler.getStitchGauge());
+        assertEquals(3, handler.getRoundGauge());
+    }
+
+    @Test
+    public void testNegativeScannerInputs() {
+        String data = "-3.1\n-5.8\n-4.6\n-3.7\n";
+        Scanner input = new Scanner(data);
+        HalfEllipsoidInputHandler handler = new HalfEllipsoidInputHandler(input);
+        assertEquals(3.1, handler.getDiameterInInches());
+        assertEquals(5.8, handler.getHeightInInches());
+        assertEquals(4.6, handler.getStitchGauge());
+        assertEquals(3.7, handler.getRoundGauge());
+    }
+
+    @Test
+    public void testGetRadiusInStitches() {
+        HalfEllipsoidInputHandler handler = new HalfEllipsoidInputHandler(3.4, 6.3, 3.3, 3.4);
+        assertEquals((3.4 / 2) * 3.3, handler.getRadiusInStitches());
+    }
+
+    @Test
+    public void testGetRadiusInRounds() {
+        HalfEllipsoidInputHandler handler = new HalfEllipsoidInputHandler(3.4, 6.3, 3.3, 3.4);
+        assertEquals((3.4 / 2) * 3.4, handler.getRadiusInRounds());
+    }
+
+    @Test
+    public void testGetHeightInStitches() {
+        HalfEllipsoidInputHandler handler = new HalfEllipsoidInputHandler(3.4, 6.3, 3.3, 3.4);
+        assertEquals(6.3 * 3.3, handler.getHeightInStitches());
+    }
+
+    @Test
+    public void testGetHeightInRounds() {
+        HalfEllipsoidInputHandler handler = new HalfEllipsoidInputHandler(3.4, 6.3, 3.3, 3.4);
+        assertEquals(6.3 * 3.4, handler.getHeightInRounds());
+    }
+
+    // TODO when update classes to handle height being less than radius, update some tests to reflect that
+}
--- app/src/test/java/com/kitrady/SphereMakerTest.java
+++ app/src/test/java/com/kitrady/SphereMakerTest.java
@@ -29,13 +29,13 @@ public class SphereMakerTest {
     public void testMakeSphere() {
         SphereMaker maker = new SphereMaker(4.6, 28.90226);
         List<Integer> stitchesPerRd = List.of(6, 12, 18, 22, 26, 28, 29, 29, 28, 26, 22, 18, 12, 6);
-        assertEquals(stitchesPerRd, maker.getStitchesPerRd());
+        assertEquals(stitchesPerRd, maker.getStitchesPerRound());
     }
 
     @Test
     public void testMakeSphere2() {
         SphereMaker maker = new SphereMaker(5, 31.4155);
         List<Integer> stitchesPerRd = List.of(6, 12, 18, 23, 26, 29, 31, 31, 31, 29, 26, 23, 18, 12, 6);
-        assertEquals(stitchesPerRd, maker.getStitchesPerRd());
+        assertEquals(stitchesPerRd, maker.getStitchesPerRound());
     }
 }
