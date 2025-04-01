0fd49e085f85523edfe46bcf8833d5e53418232f
Kit Rady <kitrady@gmail.com>
2025-04-01 15:48:03 -0500

    length larger than diameter for elongated spheres and improving input handling

--- app/src/main/java/com/kitrady/InputHelper.java
+++ app/src/main/java/com/kitrady/InputHelper.java
@@ -4,20 +4,36 @@ import java.util.Scanner;
 
 public class InputHelper {
     public static double handleInput(Scanner input, String message, String correctingMessage) {
+        double inputVal = 0;
         System.out.print(message);
 
         while (true) {
-            if (input.hasNextDouble()) {
-                return input.nextDouble();
-            }
-            if (input.hasNextInt()) {
-                return input.nextInt();
+            if (input.hasNextDouble()) { // this should prompt the scanner to wait for user input, as there should be nothing ahead of the scanner
+                inputVal = input.nextDouble(); // gets the double character from the scanner, leaving the newline character ahead of the scanner
+                input.nextLine(); // gets the newline character from the scanner, meaning there is nothing ahead of the scanner
+                return inputVal;
             }
 
+            input.nextLine(); // gets the newline character from the scanner, meaning there is nothing ahead of the scanner
             System.out.print(correctingMessage);
+        }
+    }
 
-            input.nextLine();
-            input.nextLine();
+    public static double handleInputGreaterThanValue(Scanner input, String message, String correctingMessage, double comparisonValue) {
+        double inputVal = 0;
+        System.out.print(message);
+
+        while (true) {
+            if (input.hasNextDouble()) { // this should prompt the scanner to wait for user input, as there should be nothing ahead of the scanner
+                inputVal = input.nextDouble(); // gets the double character from the scanner, leaving the newline character ahead of the scanner
+            }
+            if (Math.abs(inputVal) > comparisonValue) {
+                input.nextLine(); // gets the newline character from the scanner, meaning there is nothing ahead of the scanner
+                return inputVal;
+            }
+
+            input.nextLine(); // gets the newline character from the scanner, meaning there is nothing ahead of the scanner
+            System.out.print(correctingMessage);
         }
     }
 }
