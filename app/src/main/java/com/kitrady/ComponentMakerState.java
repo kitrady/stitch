ac49db15d65e050a66d3e2554dbd822b1f43fab0
Kit Rady <kitprady@gmail.com>
2025-06-04 15:33:57 -0500

    saving progress

---
+++ app/src/main/java/com/kitrady/ComponentMakerState.java
@@ -0,0 +1,5 @@
+package com.kitrady;
+
+public interface ComponentMakerState {
+    ComponentMakerState step(ComponentMakerInfo info);
+}
---
+++ app/src/main/java/com/kitrady/componentMakerStates/AllIncreases.java
@@ -0,0 +1,10 @@
+package com.kitrady.componentMakerStates;
+
+import com.kitrady.ComponentMakerInfo;
+import com.kitrady.ComponentMakerState;
+
+public class AllIncreases implements ComponentMakerState {
+    public ComponentMakerState step(ComponentMakerInfo info) {
+        return null;
+    }
+}
